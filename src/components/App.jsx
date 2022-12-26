df1cb34935a420264a4e6c2371bf5acc10803d04
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-26 20:00:23 +0200

    sync

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import user from '../data/users';
 import css from './App.module.css';
 
 export class App extends Component {
@@ -7,8 +8,19 @@ export class App extends Component {
       <section className={css.sectionCards}>
         <ul className={css.cardList}>
           <li className={css.cardItem}>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img className={css.logo} src="../images/logo.png" alt="logo" />
+            <img
+              className={css.cardDecor}
+              src="../images/Picture.png"
+              alt="decor"
+            />
+            <div className={css.cardPhotoWrapper}>
+              <img src="" alt="" />
+            </div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <button type="button"></button>
           </li>
         </ul>
       </section>
