5213f95b50901e45e2fea6f60eba255dc87d2e1d
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-26 20:12:03 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Component } from 'react';
-import user from '../data/users';
+import users from '../data/users';
 import css from './App.module.css';
 
 export class App extends Component {
   render() {
+    const { id, user, tweets, followers, avatar } = users[0];
     return (
       <section className={css.sectionCards}>
         <ul className={css.cardList}>
-          <li className={css.cardItem}>
+          <li key={id} className={css.cardItem}>
             <img className={css.logo} src="../images/logo.png" alt="logo" />
             <img
               className={css.cardDecor}
@@ -15,12 +16,12 @@ export class App extends Component {
               alt="decor"
             />
             <div className={css.cardPhotoWrapper}>
-              <img src="" alt="" />
+              <img src={avatar} alt="avatar" />
             </div>
-            <p></p>
-            <p></p>
-            <p></p>
-            <button type="button"></button>
+            <p>{user}</p>
+            <p>{tweets}</p>
+            <p>{followers}</p>
+            <button type="button">Follow</button>
           </li>
         </ul>
       </section>
