786e71fe4bd982022f63803a9b2ada0b65ff215a
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-27 14:57:43 +0200

    sync

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,20 +1,23 @@
 import css from './CardList.module.css';
+import { Component } from 'react';
 import { CardItem } from '../CardItem/CardItem';
 
-export const CardList = ({ users }) => {
-  return (
-    <ul className={css.cardList}>
-      {users.map(({ id, avatar, user, tweets, followers }) => {
-        return (
-          <CardItem
-            key={id}
-            avatar={avatar}
-            user={user}
-            tweets={tweets}
-            followers={followers}
-          />
-        );
-      })}
-    </ul>
-  );
-};
+export class CardList extends Component {
+  render() {
+    return (
+      <ul className={css.cardList}>
+        {this.props.users.map(({ id, avatar, user, tweets, followers }) => {
+          return (
+            <CardItem
+              key={id}
+              avatar={avatar}
+              user={user}
+              tweets={tweets}
+              followers={followers}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
+}
